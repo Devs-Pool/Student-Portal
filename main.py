a447ea5bc8b2faa0cfd41cd02127c0ea0c44fed9
Tarun Singhal <lit2016015@iiita.ac.in>
2018-03-29 15:41:25 +0530

    changes added

--- main.py
+++ main.py
@@ -13,16 +13,36 @@ class Comment(db.Model):
         self.password = password
 
 
+class Student(db.Model):
+    name = db.Column(db.String(50),primary_key = True)
+    gender = db.Column(db.String(30))
+    age = db.Column(db.Integer())
+    contact = db.Column(db.String(50))
+
+
+
 @app.route('/profile')
 
 def profile():
     return render_template("choose.html",name="Tarun")
 
+@app.route('/register',methods=['GET','POST'])
+
+def register():
+    if request.method == 'POST':
+        name  = request.form['username']
+        password = request.form['pass']
+        signature = Comment(name=name,password=password)
+        db.session.add(signature)
+        db.session.commit()
+        return render_template("thanks.html")
+    return render_template("register.html",name="nothing")
+
 
 @app.route('/form',methods=['GET','POST'])
 
 def form():
-    return render_template('register.html',name="noting")
+    return render_template('main.html',name="noting")
 
     
 @app.route('/login',methods=['GET','POST'])
@@ -33,11 +53,8 @@ def login():
         password = request.form['pass']
         person = Comment.query.all()
         for i in person:
-            if i.name == name and i.password == password:
-                return render_template('form.html',name = name)
-        signature = Comment(name=name,password=password)
-        db.session.add(signature)
-        db.session.commit()
+            if i.name == name and i.password==password:
+                return render_template('dashboard.html',name = name)
         return render_template('register.html',name = name)
     return render_template('hello.html',name="noting")
 
